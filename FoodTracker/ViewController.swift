0bce970506efbd4293353de7f749d4009fc4fc57
Dante Casanova <dante.casanova@gmail.com>
2017-04-04 15:55:33 -0500

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Dante Casanova on 21/03/17.
+//  Copyright © 2017 Dante Casanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
