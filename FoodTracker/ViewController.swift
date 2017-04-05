7e1a1b52a27caceab98f5c7eb325fc8bbdecfc74
Dante Casanova Silva <dante@MacBook-Pro-de-Dante.local>
2017-04-04 22:03:59 -0500

    from macPRO

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlet
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: Actions
 
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
